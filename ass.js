7599b3d132e28de5c6874cde5e4d43d614bcdbb6
Muinah <74907083+Muinat195@users.noreply.github.com>
2021-05-27 23:10:23 +0100

    Add files via upload

---
+++ ass.js
@@ -0,0 +1,28 @@
+//functions problem set
+
+//loops problem set 
+//using for
+number = [51];
+for (var i = 0; i < number; i = i + 2) {
+    console.log(i);
+}
+// //using while
+var i = 2;
+while ( i<51) {
+    console.log(i);
+   i-i+2;
+}
+//fizzbuzz
+var number =[101];
+for(var i = 1; i < number; i++){
+if (i%3==0){
+console.log("fizz");}
+else if(i%5==0){
+    console.log("Buzz");
+}
+else {
+    console.log(i);
+}
+}
+//iseven
+var iseven()
